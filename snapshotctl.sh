cb51b9565e5149e2dcbcd945875e843ef1bb9c1e
zawa-ch <satellite.2e1834097@gmail.com>
2023-06-22 19:04:24 +0900

    圧縮予定リストに基づいた圧縮の実装

--- snapshotctl.sh
+++ snapshotctl.sh
@@ -122,7 +122,7 @@ initialize() {
 		"CREATE TABLE \"\($db_prefix)metadata\" ( \"id\" INTEGER NOT NULL UNIQUE DEFAULT 0, \"schema_revision\" INTEGER NOT NULL, \"lock\" TEXT )",
 		"CREATE TABLE \"\($db_prefix)config\" ( \"key\" TEXT NOT NULL UNIQUE, \"value\" TEXT )",
 		"CREATE UNIQUE INDEX \"\($db_prefix)config-keys\" ON \"\($db_prefix)config\" ( \"key\" );",
-		"CREATE TABLE \"\($db_prefix)entries\" ( \"id\" INTEGER NOT NULL UNIQUE, \"date\" NUMERIC NOT NULL, \"fname\" TEXT NOT NULL UNIQUE, \"size\" INTEGER NOT NULL, \"sha256\" TEXT NOT NULL, \"type\" TEXT NOT NULL, \"depend_id\" INTEGER, PRIMARY KEY(\"id\"), FOREIGN KEY(\"depend_id\") REFERENCES \"\($db_prefix)entries\"(\"id\") ON UPDATE CASCADE )",
+		"CREATE TABLE \"\($db_prefix)entries\" ( \"id\" INTEGER NOT NULL UNIQUE, \"date\" NUMERIC NOT NULL, \"fname\" TEXT NOT NULL UNIQUE, \"size\" INTEGER NOT NULL, \"sha256\" TEXT NOT NULL, \"type\" TEXT NOT NULL, PRIMARY KEY(\"id\") )",
 		"CREATE INDEX \"\($db_prefix)entry-dates\" ON \"\($db_prefix)entries\" ( \"date\" )",
 		"CREATE UNIQUE INDEX \"\($db_prefix)entry-filenames\" ON \"\($db_prefix)entries\" ( \"fname\" )",
 		"CREATE TABLE \"\($db_prefix)keeprules\" ( \"name\" TEXT NOT NULL UNIQUE, \"store_type\" TEXT, \"bind_duration\" INTEGER, \"keep_entries\" INTEGER, \"keep_duration\" INTEGER )",
@@ -132,7 +132,7 @@ initialize() {
 		"CREATE VIEW \"\($db_prefix)keep-entry-latests\" AS SELECT * FROM \"\($db_prefix)keep-entries\" GROUP BY \"rule\" HAVING \"date\"=MAX(\"date\")",
 		"CREATE TABLE \"\($db_prefix)remove_queue\" ( \"entry_id\" INTEGER NOT NULL, FOREIGN KEY(\"entry_id\") REFERENCES \"\($db_prefix)entries\"(\"id\") ON UPDATE CASCADE ON DELETE CASCADE )",
 		"CREATE TABLE \"\($db_prefix)add_queue\" ( \"entry_id\" INTEGER NOT NULL, FOREIGN KEY(\"entry_id\") REFERENCES \"\($db_prefix)entries\"(\"id\") ON UPDATE CASCADE ON DELETE CASCADE )",
-		"CREATE TABLE \"\($db_prefix)compression_queue\" ( \"entry_id\" INTEGER NOT NULL, FOREIGN KEY(\"entry_id\") REFERENCES \"\($db_prefix)entries\"(\"id\") ON UPDATE CASCADE ON DELETE CASCADE )",
+		"CREATE TABLE \"\($db_prefix)filter_queue\" ( \"entry_id\" INTEGER NOT NULL, FOREIGN KEY(\"entry_id\") REFERENCES \"\($db_prefix)entries\"(\"id\") ON UPDATE CASCADE ON DELETE CASCADE )",
 		"INSERT INTO \"\($db_prefix)metadata\"( \"schema_revision\" ) VALUES ( \($schema_rev) )",
 		(
 			"INSERT INTO \"\($db_prefix)config\"( \"key\", \"value\" ) VALUES " + ( [
@@ -257,7 +257,7 @@ create_snapshot() {
 	local -r BACKUP_JQ_DBSTATEMENT_CREATE='( [
 		"PRAGMA journal_mode = TRUNCATE",
 		"BEGIN TRANSACTION",
-		"INSERT INTO \"\($db_prefix)entries\"( \"date\", \"fname\", \"size\", \"sha256\", \"type\", \"depend_id\" ) VALUES ( \($date), '\''\($fname)'\'', \($size), '\''\($sha256)'\'', '\''plain'\'', NULL )",
+		"INSERT INTO \"\($db_prefix)entries\"( \"date\", \"fname\", \"size\", \"sha256\", \"type\" ) VALUES ( \($date), '\''\($fname)'\'', \($size), '\''\($sha256)'\'', '\''plain'\'' )",
 		"INSERT INTO \"\($db_prefix)add_queue\"( \"entry_id\" ) VALUES ( ( SELECT MAX(\"id\") FROM \"\($db_prefix)entries\" ) )",
 		"COMMIT TRANSACTION"
 	] )|join(";")'
@@ -280,8 +280,11 @@ update_keeplist() {
 	) + (
 		$rules|map( if (.keep_duration|type) == "number" then "DELETE FROM \"\($db_prefix)keeplist\" WHERE \"rule\"='\''\(.name)'\'' AND \"entry_id\" IN ( SELECT \"id\" FROM \"\($db_prefix)keep-entries\" WHERE \"date\" < ( SELECT (\"date\"-\(.keep_duration)) FROM \"\($db_prefix)keep-entry-latests\" WHERE \"rule\"='\''\(.name)'\'' ) )" else empty end )
 	) + (
-		$rules|if length > 0 then [ "INSERT INTO \"\($db_prefix)remove_queue\"(\"entry_id\") SELECT \"id\" FROM \"\($db_prefix)entries\" WHERE \"id\" NOT IN ( SELECT \"entry_id\" FROM \"\($db_prefix)keeplist\" UNION SELECT \"entry_id\" FROM \"\($db_prefix)remove_queue\" )", "INSERT INTO \"\($db_prefix)compression_queue\"(\"entry_id\") SELECT DISTINCT \"entry_id\" FROM \"\($db_prefix)keeplist\" INNER JOIN \"\($db_prefix)keeprules\" ON \"\($db_prefix)keeprules\".\"name\"=\"\($db_prefix)keeplist\".\"rule\" WHERE \"\($db_prefix)keeprules\".\"store_type\" IS NOT NULL AND \"entry_id\" NOT IN ( SELECT \"\($db_prefix)keep-entry-latests\".\"id\" FROM \"\($db_prefix)keep-entry-latests\" UNION SELECT \"entry_id\" FROM \"\($db_prefix)compression_queue\" )" ] else [] end
+		$rules|if length > 0 then [ "INSERT INTO \"\($db_prefix)remove_queue\"(\"entry_id\") SELECT \"id\" FROM \"\($db_prefix)entries\" WHERE \"id\" NOT IN ( SELECT \"entry_id\" FROM \"\($db_prefix)keeplist\" UNION SELECT \"entry_id\" FROM \"\($db_prefix)remove_queue\" )", "INSERT INTO \"\($db_prefix)filter_queue\"(\"entry_id\") SELECT DISTINCT \"entry_id\" FROM \"\($db_prefix)keeplist\" INNER JOIN \"\($db_prefix)keeprules\" ON \"\($db_prefix)keeprules\".\"name\"=\"\($db_prefix)keeplist\".\"rule\" WHERE \"\($db_prefix)keeprules\".\"store_type\" IS NOT NULL AND \"\($db_prefix)keeprules\".\"store_type\" != '\''plain'\'' AND \"entry_id\" NOT IN ( SELECT \"entry_id\" FROM \"\($db_prefix)filter_queue\" UNION SELECT \"entry_id\" FROM \"\($db_prefix)remove_queue\" )" ] else [] end
 	) + [
+		"DELETE FROM \"\($db_prefix)add_queue\" WHERE \"entry_id\" NOT IN (SELECT \"id\" FROM \"\($db_prefix)entries\")",
+		"DELETE FROM \"\($db_prefix)remove_queue\" WHERE \"entry_id\" NOT IN (SELECT \"id\" FROM \"\($db_prefix)entries\")",
+		"DELETE FROM \"\($db_prefix)filter_queue\" WHERE \"entry_id\" NOT IN (SELECT \"id\" FROM \"\($db_prefix)entries\")",
 		"COMMIT TRANSACTION"
 	] )|join(";")'
 	local sql_statement;	sql_statement=$(jq -n -r --arg db_prefix "${BACKUP_DB_PREFIX}" --argjson rules "${rules:?}" "${BACKUP_JQ_DBSTATEMENT_UPDATE:?}") || { local rcode=$?; rel_lock "${lock_code:?}"; return $rcode; }
@@ -338,6 +341,8 @@ process_remove_queue_item() {
 	# shellcheck disable=SC2016
 	local -r BACKUP_JQ_DBSTATEMENT_PROCESS_REMOVE_ITEM='( [
 		"PRAGMA journal_mode = TRUNCATE",
+		"DELETE FROM \"\($db_prefix)add_queue\" WHERE \"entry_id\"=\($entry_id)",
+		"DELETE FROM \"\($db_prefix)filter_queue\" WHERE \"entry_id\"=\($entry_id)",
 		"DELETE FROM \"\($db_prefix)remove_queue\" WHERE \"entry_id\"=\($entry_id)",
 		"DELETE FROM \"\($db_prefix)entries\" WHERE \"id\"=\($entry_id)"
 	] )|join(";")'
@@ -351,6 +356,119 @@ process_remove_queue() {
 	[ -n "$remove_queue" ] || remove_queue='[]'
 	for item in $(jq -n -r --argjson remove_queue "$remove_queue" '$remove_queue|.[]'); do
 		process_remove_queue_item "$item" || return
+	done
+}
+
+filter_encode() {
+	local lock_code=$1
+	local entry_id=$2
+	[ "$(sqlite3 -readonly "${BACKUP_DB_PATH:?}" "SELECT COUNT(*) FROM \"${BACKUP_DB_PREFIX}filter_queue\" WHERE \"entry_id\"=${entry_id:?}")" -gt 0 ] || { echo '{"return_code":1,"error":{"code":"ENTRY_NOT_EXIST","message":"Entry '"${entry_id}"' does not exist."}}'; return; }
+	local fname=$3
+	local filter=$4
+	local db_location;	db_location=$(cd "$(dirname "${BACKUP_DB_PATH:?}")" && pwd) || { echo '{"error":{"code":"CANNOT_LOCATE_DATABASE_LOCATION","message":"Couldn'\''t locate DB location."}}'; return; }
+	local backup_destination;	backup_destination=$(get_config | jq -r --arg db_location "${db_location:?}" '.backup_destination|if startswith("/") then . else ("\($db_location)/" + .) end') || { echo '{"error":{"code":"CANNOT_LOCATE_SNAPSHOT_DIRECTORY","message":"Couldn'\''t locate snapshot directory."}}'; return; }
+	local worktmp;	worktmp=$(get_config | jq -r --arg db_location "${db_location:?}" '.worktmp|if startswith("/") then . else ("\($db_location)/" + .) end') || { echo '{"error":{"code":"CANNOT_LOCATE_WORKTMP","message":"Couldn'\''t locate working temp directory."}}'; return; }
+	case $filter in
+	plain)	echo '{"error":null,"new_fname":"'"${fname:?}"'"}'; return;;
+	gzip)
+		do_lock "${lock_code:?}" gzip --suffix=.gz "${worktmp:?}/${fname:?}" || return
+		echo '{"error":null,"new_fname":"'"${fname}"'.gz"}'
+		return;;
+	zstd)
+		do_lock "${lock_code:?}" zstd --rm "${worktmp:?}/${fname:?}" -o "${worktmp:?}/${fname:?}.zst" || return
+		echo '{"error":null,"new_fname":"'"${fname}"'.zst"}'
+		return;;
+	xz)
+		do_lock "${lock_code:?}" xz "${worktmp:?}/${fname:?}" || return
+		echo '{"error":null,"new_fname":"'"${fname}"'.xz"}'
+		return;;
+	*)	echo '{"error":{"code":"INVALID_FILTER","message":"Specified invalid filter"}}'; return;;
+	esac
+}
+
+filter_decode() {
+	local lock_code=$1
+	local entry_id=$2
+	[ "$(sqlite3 -readonly "${BACKUP_DB_PATH:?}" "SELECT COUNT(*) FROM \"${BACKUP_DB_PREFIX}filter_queue\" WHERE \"entry_id\"=${entry_id:?}")" -gt 0 ] || { echo '{"return_code":1,"error":{"code":"ENTRY_NOT_EXIST","message":"Entry '"${entry_id}"' does not exist"}}'; return 1; }
+	local fname=$3
+	local filter=$4
+	local db_location;	db_location=$(cd "$(dirname "${BACKUP_DB_PATH:?}")" && pwd) || { echo '{"error":{"code":"CANNOT_LOCATE_DATABASE_LOCATION","message":"Couldn'\''t locate DB location"}}'; return 1; }
+	local backup_destination;	backup_destination=$(get_config | jq -r --arg db_location "${db_location:?}" '.backup_destination|if startswith("/") then . else ("\($db_location)/" + .) end') || { echo '{"error":{"code":"CANNOT_LOCATE_SNAPSHOT_DIRECTORY","message":"Couldn'\''t locate snapshot directory."}}'; return 1; }
+	local worktmp;	worktmp=$(get_config | jq -r --arg db_location "${db_location:?}" '.worktmp|if startswith("/") then . else ("\($db_location)/" + .) end') || { echo '{"error":{"code":"CANNOT_LOCATE_WORKTMP","message":"Couldn'\''t locate working temp directory."}}'; return 1; }
+	case $filter in
+	plain)	echo '{"error":null,"new_fname":"'"${fname:?}"'"}'; return;;
+	gzip)
+		do_lock "${lock_code:?}" gunzip --suffix=.gz "${worktmp:?}/${fname:?}" || return
+		echo '{"error":null,"new_fname":"'"${fname%.gz}"'"}'
+		return;;
+	zstd)
+		do_lock "${lock_code:?}" zstd --decompress --rm "${worktmp:?}/${fname:?}" -o "${worktmp:?}/${fname%.zst}" || return
+		echo '{"error":null,"new_fname":"'"${fname%.zst}"'"}'
+		return;;
+	xz)
+		do_lock "${lock_code:?}" xz --decompress "${worktmp:?}/${fname:?}" || return
+		echo '{"error":null,"new_fname":"'"${fname%.xz}"'"}'
+		return;;
+	*)	echo '{"error":{"code":"INVALID_FILTER","message":"Specified invalid filter"}}'; return 1;;
+	esac
+}
+
+process_filter_queue_item() {
+	local entry_id=$1
+	[ "$(sqlite3 -readonly "${BACKUP_DB_PATH:?}" "SELECT COUNT(*) FROM \"${BACKUP_DB_PREFIX}filter_queue\" WHERE \"entry_id\"=${entry_id:?}")" -gt 0 ] || { echo "snapshotctl: Not exist entry ${entry_id} from compression queue" >&2; return 1; }
+	local db_location;	db_location=$(cd "$(dirname "${BACKUP_DB_PATH:?}")" && pwd) || return
+	local backup_destination;	backup_destination=$(get_config | jq -r --arg db_location "${db_location:?}" '.backup_destination|if startswith("/") then . else ("\($db_location)/" + .) end') || return
+	local worktmp;	worktmp=$(get_config | jq -r --arg db_location "${db_location:?}" '.worktmp|if startswith("/") then . else ("\($db_location)/" + .) end') || return
+	local convert_type;	convert_type=$(sqlite3 -json -readonly "${BACKUP_DB_PATH:?}" "SELECT \"${BACKUP_DB_PREFIX}keeprules\".* FROM \"${BACKUP_DB_PREFIX}keeplist\" LEFT JOIN \"${BACKUP_DB_PREFIX}keeprules\" ON \"${BACKUP_DB_PREFIX}keeplist\".\"rule\" = \"${BACKUP_DB_PREFIX}keeprules\".\"name\" WHERE \"entry_id\" = ${entry_id:?}" | jq -r 'def is_plain: (type == "null") or (type == "string" and . == "plain"); def is_compress: (type == "string") and ( . as $value|[ "gzip", "zstd", "xz" ]|map(. == $value)|any ); def is_difference: (type == "string") and (split("|")|.[0] == "rdiff") and (split("|")|map(is_plain or is_compress or . == "rdiff")|all); def is_valid: is_plain or is_compress or is_difference; if (map(.store_type|is_valid|not)|any) then ("snapshotctl: Invalid store type found. Please check config.\n"|halt_error) elif (map(.store_type|is_plain)|any) then "plain" elif (map(.store_type|is_compress)|any) then (map(select(.store_type|is_compress))|sort_by(.bind_duration)|reverse|.[0].store_type) else (sort_by(.bind_duration)|reverse|.[0].store_type) end') || return
+	local fname;	fname=$(sqlite3 -readonly "${BACKUP_DB_PATH:?}" "SELECT \"fname\" FROM \"${BACKUP_DB_PREFIX}entries\" WHERE \"id\"=${entry_id:?}") || return
+	local item_type;	item_type=$(sqlite3 -readonly "${BACKUP_DB_PATH:?}" "SELECT \"type\" FROM \"${BACKUP_DB_PREFIX}entries\" WHERE \"id\"=${entry_id:?}") || return
+	if [ "${item_type:?}" == "${convert_type:?}" ]; then
+		sqlite3 "${BACKUP_DB_PATH:?}" "PRAGMA journal_mode = TRUNCATE;DELETE FROM \"${BACKUP_DB_PREFIX}filter_queue\" WHERE \"entry_id\"=${entry_id:?}" >/dev/null
+		return
+	fi
+	local lock_code;	lock_code=$(cat <(echo "COMPRESS:${entry_id:?}:") <(head --bytes=8 -q /dev/urandom) | sha256sum -b - | awk '{ print $1 }') || return
+	acq_lock "${lock_code:?}" || return
+	if [ -e "${worktmp:?}" ]; then
+		do_lock "${lock_code:?}" rm -rf "${worktmp:?}/*" || { local rcode=$?; rel_lock "${lock_code:?}"; return $rcode; }
+	else
+		do_lock "${lock_code:?}" mkdir -p "${worktmp:?}" || { local rcode=$?; rel_lock "${lock_code:?}"; return $rcode; }
+	fi
+	do_lock "${lock_code:?}" cp -f -t "${worktmp:?}" "${backup_destination:?}/${fname:?}" || { local rcode=$?; rel_lock "${lock_code:?}"; return $rcode; }
+	local new_fname="${fname:?}"
+	for filter in $(echo "${item_type:?}" | jq -Rr 'split("|")|reverse|.[]')
+	do
+		local result;	result=$(filter_decode "${lock_code:?}" "${entry_id:?}" "${new_fname:?}" "${filter:?}") || { local rcode=$?; rel_lock "${lock_code:?}"; return $rcode; }
+		echo -E "${result:?}" | jq -r 'if (.error|type) != "null" then ("snapshotctl: Filter returned with error \(.error.code)\n\(.error.message)"|halt_error) else empty end' || { local rcode=$?; rel_lock "${lock_code:?}"; return $rcode; }
+		new_fname=$(echo -E "${result:?}" | jq -r --arg fname "${new_fname:?}" '.new_fname // $fname')
+	done
+	for filter in $(echo "${convert_type:?}" | jq -Rr 'split("|")|.[]')
+	do
+		local result;	result=$(filter_encode "${lock_code:?}" "${entry_id:?}" "${new_fname:?}" "${filter:?}") || { local rcode=$?; rel_lock "${lock_code:?}"; return $rcode; }
+		echo -E "${result:?}" | jq -r 'if (.error|type) != "null" then ("snapshotctl: Filter returned with error \(.error.code)\n\(.error.message)"|halt_error) else empty end' || { local rcode=$?; rel_lock "${lock_code:?}"; return $rcode; }
+		new_fname=$(echo -E "${result:?}" | jq -r --arg fname "${new_fname:?}" '.new_fname // $fname')
+	done
+	local size;	size=$(do_lock "${lock_code:?}" wc -c "${worktmp:?}/${new_fname:?}" | awk '{ print $1 }') || { local rcode=$?; rel_lock "${lock_code:?}"; return $rcode; }
+	local checksum;	checksum=$(do_lock "${lock_code:?}" sha256sum -b "${worktmp:?}/${new_fname:?}" | awk '{ print $1 }') || { local rcode=$?; rel_lock "${lock_code:?}"; return $rcode; }
+	do_lock "${lock_code:?}" mv --no-clobber --target-directory="${backup_destination:?}/" "${worktmp:?}/${new_fname:?}" || { local rcode=$?; rel_lock "${lock_code:?}"; return $rcode; }
+	# shellcheck disable=SC2016
+	local -r BACKUP_JQ_DBSTATEMENT_PROCESS_FILTER='( [
+		"PRAGMA journal_mode = TRUNCATE",
+		"BEGIN TRANSACTION",
+		"UPDATE \"\($db_prefix)entries\" SET \"fname\" = '\''\($new_fname)'\'', \"size\" = \($size), \"sha256\" = '\''\($sha256)'\'', \"type\" = '\''\($new_type)'\'' WHERE \"id\" = \($entry_id)",
+		"DELETE FROM \"\($db_prefix)filter_queue\" WHERE \"entry_id\" = \($entry_id)",
+		"COMMIT TRANSACTION"
+	] )|join(";")'
+	local sql_statement;	sql_statement=$(jq -n -r --arg db_prefix "${BACKUP_DB_PREFIX}" --argjson entry_id "${entry_id:?}" --arg new_fname "${new_fname:?}" --argjson size "${size:?}" --arg sha256 "${checksum:?}" --arg new_type "${convert_type:?}" "${BACKUP_JQ_DBSTATEMENT_PROCESS_FILTER:?}") || { local rcode=$?; rm -f "${backup_destination:?}/${new_fname:?}"; rel_lock "${lock_code:?}"; return $rcode; }
+	sqlite3 "${BACKUP_DB_PATH:?}" "${sql_statement:?}" >/dev/null || { local rcode=$?; rm -f "${backup_destination:?}/${new_fname:?}"; rel_lock "${lock_code:?}"; return $rcode; }
+	rm -f "${backup_destination:?}/${fname:?}"
+	rel_lock "${lock_code:?}"
+}
+
+process_filter_queue() {
+	local filter_queue;	filter_queue=$(sqlite3 -json -readonly "${BACKUP_DB_PATH:?}" "SELECT DISTINCT \"entry_id\" FROM \"${BACKUP_DB_PREFIX}filter_queue\"" | jq -c 'map(.entry_id)') || return
+	[ -n "$filter_queue" ] || filter_queue='[]'
+	for item in $(jq -n -r --argjson filter_queue "$filter_queue" '$filter_queue|.[]'); do
+		process_filter_queue_item "$item" || return
 	done
 }
 
@@ -482,6 +600,7 @@ command_update() {
 	process_add_queue || return
 	update_keeplist || return
 	process_remove_queue || return
+	process_filter_queue || return
 }
 
 command_clean() {
