7bf61d229f060c053ccc66059aefeadf1d526c66
zawa-ch <satellite.2e1834097@gmail.com>
2023-06-22 22:00:27 +0900

    データベースのプレフィックスを変更
    backup- -> snapshotctl-

--- snapshotctl.sh
+++ snapshotctl.sh
@@ -25,7 +25,7 @@ __LICENSE__
 
 # SNAPSHOTCTL_BIN_LOCATION="$(cd "$(dirname "$0")" && pwd)" || exit
 # readonly SNAPSHOTCTL_BIN_LOCATION
-readonly SNAPSHOTCTL_DB_PREFIX='backup-'
+readonly SNAPSHOTCTL_DB_PREFIX='snapshotctl-'
 readonly SNAPSHOTCTL_DB_SCHEMA_REVISION=1
 
 #	---- 設定項目 ----
